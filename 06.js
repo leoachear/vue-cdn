fcd277ea22d31dbf69eedc469ec35a35d174a36c
@leoachear <leoachear@gmail.com>
2020-05-17 11:49:56 -0300

    Pruebas con el CDN de Vue.js

---
+++ 06.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        mensaje: 'hola soy el mensaje',
+        contador: 0,
+    },
+    methods: {
+    },
+    computed:{
+        invertido(){
+            return this.mensaje.split('').reverse().join('');
+        },
+        color(){
+            return {
+                'bg-success' : this.contador <= 10,
+                'bg-warning' : this.contador > 10 && this.contador < 20,
+                'bg-danger' : this.contador >= 20,
+            };
+        }
+    }
+})
